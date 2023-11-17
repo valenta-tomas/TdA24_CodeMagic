bfc6f861ecdef8885091e785de6521da94a4a6c5
garelsn <67955168+garelsn@users.noreply.github.com>
2023-11-17 13:20:44 +0100

    Update index.js, add api

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  fetch("http://server-url.cz/api")
+  .then((res) => res.json())
+  .then(data=> console.log(data))
+
+  .catch(function (err) {
+    console.log("Unable to fetch -", err);
+  });
+
   res.render('index', { title: 'Express' });
 });
 
