c9fa5ad5f9f37c38ed54dd99a833f586acc1fc91
garelsn <67955168+garelsn@users.noreply.github.com>
2023-11-17 16:58:33 +0100

    Update index.js get api

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,13 @@
 var express = require('express');
 var router = express.Router();
-const api= {
-  "secret":"The cake is a lie"
-}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
 router.get('/api',(req, res)=>{
-  console.log(api)
+  res.json({secret:"The cake is a lie"});
 })
 
 module.exports = router;
