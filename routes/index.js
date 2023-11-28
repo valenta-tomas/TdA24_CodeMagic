ad8dc8f70cadcfa028cc184ff91c59b92478baca
garelsn <kkitr37@gmail.com>
2023-11-28 17:32:17 +0100

    edit css
    edit css

--- routes/index.js
+++ routes/index.js
@@ -21,6 +21,14 @@ router.get('/lecturer',(req, res)=>{
     ,bio:data.bio
     ,telephone_numbers:data.contact.telephone_numbers
     ,emails:data.contact.emails
+    ,tag1 : data.tags[0].name
+    ,tag2 : data.tags[1].name
+    ,tag3 : data.tags[2].name
+    ,tag4 : data.tags[3].name
+    ,tag5 : data.tags[4].name
+    ,tag6 : data.tags[5].name
+    ,tag7 : data.tags[6].name
+    ,tag8 : data.tags[7].name
    });
 })
 module.exports = router;
