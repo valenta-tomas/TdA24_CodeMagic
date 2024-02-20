859eb7391dd07ee2e0b1968b42a310e195245473
garelsn <kkitr37@gmail.com>
2024-02-20 14:08:30 +0100

    date
    date

--- public/scripts/myDateTime.js
+++ public/scripts/myDateTime.js
@@ -1,5 +1,11 @@
+const myDate = document.getElementById("myDate")
+const date = new Date();
 
-const date = new Date()
+const year = date.getFullYear();
+const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Přidání nuly pro jednociferný měsíc
+const day = date.getDate().toString().padStart(2, '0'); // Přidání nuly pro jednociferný den
 
-const h = date.getHours()
-console.log(h)
+const formattedDate = `${year}-${month}-${day}`;
+
+myDate.value = formattedDate;
+myDate.min = formattedDate;
